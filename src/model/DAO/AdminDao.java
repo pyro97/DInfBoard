a109a7e0c4050aef6f836bff49d92d53f1c2c086
Antonio Junior <antoniojjcitarella@hotmai.it>
2018-12-15 01:58:38 +0100

    Aggiunti oggetti Admin

---
+++ src/model/DAO/AdminDao.java
@@ -0,0 +1,29 @@
+package model.DAO;
+import model.PJO.*;
+
+public class AdminDao {
+
+	public AdminDao() {
+		
+	}
+	
+	public boolean inserisciAdmin(Admin a) {
+		
+		return true;
+	}
+	
+	public boolean eliminaAdmin(Admin a) {
+		
+		return true;
+	}
+	
+	public boolean eliminaAnnuncio(Annuncio a) {
+		
+		return true;
+	}
+	
+	public boolean sospendiUtente(Studente s) {
+		
+		return true;
+	}
+}
