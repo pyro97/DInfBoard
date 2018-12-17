f88e52d3e8017c4135e4384b1261529d3d826597
Antonio Junior <antoniojjcitarella@hotmai.it>
2018-12-17 21:37:10 +0100

    Aggiunta classe Main prova dell'utilizzatore di password

--- src/model/PJO/Main.java
+++ src/model/PJO/Main.java
@@ -4,7 +4,30 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		/*
+		String password = "provaPassword";
+		String salt = PasswordUtils.getSalt(10);
 		
+		String mySecurePassword = PasswordUtils.generateSecurePassword(password, salt);
+		
+		System.out.println(" password generata " + mySecurePassword);
+		System.out.println(" Valore salt " + salt);
+		*/
+		
+		String providedPassword = "provaPassword";
+		
+		String securePassword = "n01AmDNcMhP3aaTMAjxlDtHHUXHuL++c1rDTON/MSwo=";
+		
+		String salt = "1OX74xl4qw";
+		
+		boolean passwordMatch = PasswordUtils.verifyUserPassword(providedPassword, securePassword, salt);
+		
+		if(passwordMatch) {
+			System.out.println("Password combacia");
+		}
+		else {
+			System.out.println("Password non combacia");
+		}
 	}
 
 }
