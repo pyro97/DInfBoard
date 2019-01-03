51ff0674d2067c1739eeff914c51a9a6f3f23e32
DINFBoard <45486132+DINFBoard@users.noreply.github.com>
2019-01-03 19:47:02 +0100

    Delete popolamentoData.sql

--- popolamentoData.sql
+++
@@ -1,7 +0,0 @@
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(0,"Nessuna_Preferenza");
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(1,"Gruppo di Progetto");
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(2,"Matematica");
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(3,"Programmazione");
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(4,"CyberSecurity");
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(5,"Linux/Open Software");
-Insert into Preferenze(ID_Preferenza,Nome_Preferenza) values(6,"Gruppo di Studio");
