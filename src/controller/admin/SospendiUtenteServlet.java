ba48045e8d989803b5479c5061609b4b49997e36
DINFBoard <45486132+DINFBoard@users.noreply.github.com>
2019-01-06 16:41:02 +0100

    aggiornato sospendiutenteservlet

--- src/controller/admin/SospendiUtenteServlet.java
+++ src/controller/admin/SospendiUtenteServlet.java
@@ -11,39 +11,25 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.DAO.implement.ManagerAdmin;
 
-/**
- * Servlet implementation class SospendiUtenteServlet
- */
 @WebServlet("/SospendiUtenteServlet")
 public class SospendiUtenteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public SospendiUtenteServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("usernameUtente");
 		if(ManagerAdmin.sospendiUtente(username)) {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("success.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("adminArea.jsp");
 			dispatcher.forward(request, response);
 		}else {
 			response.sendRedirect("error.jsp");
 		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
