f2696bb929540c5c07a2a77086d5f598e59da293
Antonio <antoniojjcitarella@hotmail.com>
2019-01-21 12:05:50 +0100

    aggiunta package test

--- src/test/JUnitTestManagerPreferenza.java
+++ src/test/JUnitTestManagerPreferenza.java
@@ -2,13 +2,47 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
-class JUnitTestManagerPreferenza {
+import model.DAO.PreferenzaDao;
+import model.DAO.implement.ManagerPreferenza;
+import model.DAO.implement.ManagerStudente;
+import model.PJO.Preferenza;
+import model.PJO.Studente;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+class JUnitTestManagerPreferenza {
+	@BeforeClass
+	  public static void initClass() {
+	    System.out.println("initClass()");
+	  }
+	  @AfterClass
+	  public static void endClass() {
+	    System.out.println("endClass()");
+	  }
+	  @Before
+	  public void initMethod() {
+	    System.out.println("initMethod()");
+	  }
+	  @After
+	  public void endMethod() {
+	    System.out.println("end Method");
+	  }
+	  @Test
+	  public void testGetAllPreferenze() {
+	    System.out.println("Test getAllPreferenze");
+	    Preferenza p=new Preferenza(2121,"programmazione");
+	    PreferenzaDao dao=new PreferenzaDao();
+	    dao.add(p);
+	    ArrayList<Preferenza> pref=new ArrayList<>();
+	    pref.add(p);
+	    
+	    assertEquals(pref,ManagerPreferenza.getElencoPreferenze());
+	  }
 
 }
